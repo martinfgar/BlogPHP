50b113c5e89984cfbd91259c11ff4230fc356183
Martin <martinfifa2@gmail.com>
2022-11-10 09:28:36 +0100

    edit alt

--- Views/partials/header/admin.php
+++ Views/partials/header/admin.php
@@ -1,2 +1,2 @@
 <a href="/userform" style="margin-left: 1em;"><img src="images/user-add.svg" alt="New User" title="New user"></a>
-<a href="/adminPanel" style="margin-left: 1em;"><img src="images/table-icon.svg" alt="All User" title="All user"></a>
+<a href="/adminPanel" style="margin-left: 1em;"><img src="images/table-icon.svg" alt="Admin Panel" title="Admin Panel"></a>
